9499fc51c5cecbbe05d07a138d9449571b13791f
Kobold831 <146227823+Kobold831@users.noreply.github.com>
2024-03-03 17:40:46 +0900

    修正

--- app/src/main/java/com/saradabar/cpadcustomizetool/Receiver/BootCompletedReceiver.java
+++ app/src/main/java/com/saradabar/cpadcustomizetool/Receiver/BootCompletedReceiver.java
@@ -1,3 +1,17 @@
+/* CPad Customize Tool
+ * Copyright © 2021-2024 Kobold831 <146227823+kobold831@users.noreply.github.com>
+ *
+ * CPad Customize Tool（以下本ソフトウェアという）はオープンソフトウェアです。
+ * これは、Apacheソフトウェア財団 によって発行された Apache License 2.0 （以下本ライセンスという）の条件に基づいています。
+ * 本ソフトウェアの著作権法に定義される利用は本ライセンスに定義された範囲でいかなる行為をすることができます。
+ *
+ * Kobold831（以下著作権者という）は著作権法に定義されるこのプロジェクト全体の著作物（以下著作物という）の、
+ * 著作権法に定義される著作権（以下著作権という）かつ著作権法に定義される著作人格権を有しておりまた放棄していません。
+ * 本ソフトウェアを本ライセンスの範囲を超えて使用、複製、配布された場合、
+ * 侵害行為地の著作権法が適用され著作権者は著作権法で定義される差止請求権を行使して著作権法に定義される差止請求を行います。
+ *
+ */
+
 package com.saradabar.cpadcustomizetool.Receiver;
 
 import android.content.BroadcastReceiver;
